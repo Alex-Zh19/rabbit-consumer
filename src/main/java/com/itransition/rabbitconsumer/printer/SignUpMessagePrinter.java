8ae7fd43b8e859067f310012ea747a04f6c89da8
alexandr <alexzhalnerchikz@mail.ru>
2021-09-17 00:16:35 +0300

    formation fixed

--- src/main/java/com/itransition/rabbitconsumer/printer/SignUpMessagePrinter.java
+++ src/main/java/com/itransition/rabbitconsumer/printer/SignUpMessagePrinter.java
@@ -1,30 +1,13 @@
 package com.itransition.rabbitconsumer.printer;
 
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.io.UnsupportedEncodingException;
 
 @Component
 public class SignUpMessagePrinter {
 
-    private final RabbitTemplate template;
-    private final String ROUTING_KEY = "rrrrrrrrrrouting";
-
-    @Autowired
-    public SignUpMessagePrinter(RabbitTemplate template) {
-        this.template = template;
-    }
-
     @RabbitListener(queues = "messageBrokerQueue")
     public void worker1(String message) {
-     /*   Message me = template.receive();
-        System.out.println(new String(me.getBody(), "UTF-8") + "    hurrey");*/
-        System.out.println(message+" hurrey");
+        System.out.println(message);
     }
-
-
 }
--- src/main/java/com/itransition/rabbitconsumer/rabbitconfig/RabbitConfiguration.java
+++ src/main/java/com/itransition/rabbitconsumer/rabbitconfig/RabbitConfiguration.java
@@ -1,56 +1,21 @@
 package com.itransition.rabbitconsumer.rabbitconfig;
-
-import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 @EnableRabbit
 @Configuration
 public class RabbitConfiguration {
+    @Value("${rabbit.host}")
+    private String HOST;
 
     @Bean
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory connectionFactory =
-                new CachingConnectionFactory("localhost");
+                new CachingConnectionFactory(HOST);
         return connectionFactory;
-    }}
-/*
-    @Bean
-    public AmqpAdmin amqpAdmin() {
-        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory());
-        return rabbitAdmin;
     }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate() {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
-        rabbitTemplate.setExchange(exchange);
-        return rabbitTemplate;
-    }
-
-    @Bean
-    public Queue myQueue1() {
-        return new Queue(QUEUE_NAME);
-    }
-
-   */
-/* @Bean
-    public DirectExchange directExchange(){
-        return new DirectExchange(exchange);
-    }*//*
-
-
-   */
-/* @Bean
-    public Binding errorBinding1(){
-        return BindingBuilder.bind(myQueue1()).to(directExchange()).with(ROUTING_KEY);
-    }*//*
-
-
-}*/
+}
