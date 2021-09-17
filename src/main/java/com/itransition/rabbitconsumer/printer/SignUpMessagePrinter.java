63572f1aaf6c75817a260e6c65a39bb73b52f899
alexandr <alexzhalnerchikz@mail.ru>
2021-09-17 09:04:14 +0300

    log4j added, config to log4j added

--- src/main/java/com/itransition/rabbitconsumer/printer/SignUpMessagePrinter.java
+++ src/main/java/com/itransition/rabbitconsumer/printer/SignUpMessagePrinter.java
@@ -1,13 +1,18 @@
 package com.itransition.rabbitconsumer.printer;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SignUpMessagePrinter {
 
+    private static final Logger logger = LogManager.getLogger();
+
     @RabbitListener(queues = "messageBrokerQueue")
     public void worker1(String message) {
-        System.out.println(message);
+        logger.log(Level.INFO, message);
     }
 }
