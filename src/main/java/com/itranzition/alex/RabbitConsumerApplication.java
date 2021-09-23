cad3820abc35d75a936c1671b4f49459b6ddda73
alexandr <alexzhalnerchikz@mail.ru>
2021-09-23 11:01:16 +0300

    formation fixed, versions at pom added to properties

--- src/main/java/com/itranzition/alex/RabbitConsumerApplication.java
+++ src/main/java/com/itranzition/alex/RabbitConsumerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RabbitConsumerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RabbitConsumerApplication.class, args);
     }
-
 }
