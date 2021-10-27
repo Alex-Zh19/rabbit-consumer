1394a3e49b6dfc47fc53b92e19ed0132f5f724fb
alexandr <alexzhalnerchikz@mail.ru>
2021-10-27 18:37:50 +0300

    add RabbitConfigurationProperties

--- src/main/java/com/itranzition/alex/RabbitConsumerApplication.java
+++ src/main/java/com/itranzition/alex/RabbitConsumerApplication.java
@@ -1,9 +1,12 @@
 package com.itranzition.alex;
 
+import com.itranzition.alex.properties.RabbitConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({RabbitConfigurationProperties.class})
 public class RabbitConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(RabbitConsumerApplication.class, args);
